73a97d0c12a80d197aec458921f0553c722fa9e9
jellan bozo <jellan.bozo@walmart.com>
2021-05-30 13:34:46 -0400

    add page login and remove login

--- src/pages/SingUp.js
+++ src/pages/SingUp.js
@@ -56,7 +56,7 @@ export default function SignUp() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Crear cuenta
+          Crear Cuenta
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
