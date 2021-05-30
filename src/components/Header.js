9c8c3d2c23084db94d56560f9786b2cc751ca96f
jellan bozo <jellan.bozo@walmart.com>
2021-05-30 13:21:57 -0400

    add some page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import LinkMatereial from '@material-ui/core/Link';
+import CardMedia from '@material-ui/core/CardMedia';
+import { Link } from "react-router-dom";
+
+
+import imageLogo from '../images/logoweb.jpg'
 
 const useStyles = makeStyles((theme) => ({
     toolbar: {
@@ -21,33 +26,50 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(1),
         flexShrink: 0,
     },
+      media: {
+        paddingTop: '7%', // 16:9
+        background: 'transparent',
+        backgroundRepeat: 'no-repeat',
+        marginLeft: '30%'
+      },
 }));
 
 export default function Header(props) {
     const classes = useStyles();
-    const { sections, title } = props;
+    const { sections } = props;
 
     return (
         <React.Fragment>
             <Toolbar className={classes.toolbar}>
-                <Button size="small">Registrarse</Button>
+            <Link style={{ textDecoration: 'none', color: 'black' }} to='/SingUp'>
+                    <Button size="small" variant="outlined">                    
+                        Crear Cuenta                   
+                    </Button>
+                </Link>
                 <Typography
                     component="h2"
                     variant="h5"
                     color="inherit"
                     align="center"
                     noWrap
-                    className={classes.toolbarTitle}
-                >
-                    {title}
+                    className={classes.toolbarTitle}                    
+                >                     
+                        <CardMedia
+                        align="center"
+                            className={classes.media}
+                            image={imageLogo}
+                        />     
+                    
                 </Typography>
-                <Button variant="outlined" size="small">
-                    Iniciar Sesion
-                </Button>
+                {/* <Link style={{ textDecoration: 'none', color: 'black' }} to='/SingIn'>
+                    <Button size="small" variant="outlined">                    
+                        Iniciar Sesion                    
+                    </Button>
+                </Link> */}
             </Toolbar>
             <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
                 {sections.map((section) => (
-                    <Link
+                    <LinkMatereial
                         color="inherit"
                         noWrap
                         key={section.title}
@@ -56,7 +78,7 @@ export default function Header(props) {
                         className={classes.toolbarLink}
                     >
                         {section.title}
-                    </Link>
+                    </LinkMatereial>
                 ))}
             </Toolbar>
         </React.Fragment>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,38 +1,118 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import Markdown from '../components/Markdown';
+import Container from '@material-ui/core/Container';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+import Header from '../components/Header';
+import MainFeaturedPost from '../components/MainFeaturedPost';
+import FeaturedPost from '../components/FeaturePost';
+// import Main from '../src/pages/Main';
+// import Sidebar from '../src/components/Siderbar';
+import Footer from '../components/Footer';
+// import post1 from '../src/utils/blog-post.1.md';
+// import post2 from '../src/utils/blog-post.2.md';
+// import post3 from '../src/utils/blog-post.3.md';
 
-const useStyles = makeStyles((theme) => ({
-    markdown: {
-        ...theme.typography.body2,
-        padding: theme.spacing(3, 0),
+import imagenCenter from '../images/SRO-Preview.jpeg'
+import imgCommingzoon from '../images/silkroadProximamente.jpg'
+import imgUpdate from '../images/silkroadActualizacion.jpg'
+import { Fragment } from 'react';
+
+// const useStyles = makeStyles((theme) => ({
+//     mainGrid: {
+//         marginTop: theme.spacing(3),
+//     },
+// }));
+
+const sections = [
+    { title: 'noticias', url: '#' },
+    { title: 'comunidad', url: '#' },
+    { title: 'descargar', url: '#' },
+    { title: 'Politicas', url: '#' }
+];
+
+const mainFeaturedPost = {
+    title: 'Bienvenido a Silkroad Allianz',
+    description:
+        "",
+    image: imagenCenter,
+    imgText: 'main image description',
+};
+
+const featuredPosts = [
+    {
+        title: 'Proximos avances',
+        date: 'Junio 30',
+        description:
+            'Las siguientes actualizaciones tienen que ver con la información que sera mostrada el este sitio web donde abra noticias y encuestas de como podrian cambiar la jugabilidad',
+        image: imgCommingzoon,
+        imageText: 'Image Text',
     },
-}));
+    {
+        title: 'Avances del juego',
+        date: 'Junio 30',
+        description:
+            'Estaremos comentando los avances y mejoras que se les esten haciendo al servidor con la intención de recibir comentarios de que tan buenos fueron las mejoras',
+        image: imgUpdate,
+        imageText: 'Image Text',
+    },
+];
 
-export default function Main(props) {
-    const classes = useStyles();
-    const { posts, title } = props;
+// const posts = [post1, post2, post3];
 
-    return (
-        <Grid item xs={12} md={8}>
-            <Typography variant="h6" gutterBottom>
-                {title}
-            </Typography>
-            <Divider />
-            {posts.map((post) => (
-                <Markdown className={classes.markdown} key={post.substring(0, 40)}>
-                    {post}
-                </Markdown>
-            ))}
-        </Grid>
-    );
+// const sidebar = {
+//     title: 'About',
+//     description:
+//         'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
+//     archives: [
+//         { title: 'March 2020', url: '#' },
+//         { title: 'February 2020', url: '#' },
+//         { title: 'January 2020', url: '#' },
+//         { title: 'November 1999', url: '#' },
+//         { title: 'October 1999', url: '#' },
+//         { title: 'September 1999', url: '#' },
+//         { title: 'August 1999', url: '#' },
+//         { title: 'July 1999', url: '#' },
+//         { title: 'June 1999', url: '#' },
+//         { title: 'May 1999', url: '#' },
+//         { title: 'April 1999', url: '#' },
+//     ],
+//     social: [
+//         { name: 'GitHub', icon: GitHubIcon },
+//         { name: 'Twitter', icon: TwitterIcon },
+//         { name: 'Facebook', icon: FacebookIcon },
+//     ],
+// };
+
+export default function Main(){
+// const classes = useStyles();
+    return(
+        <Fragment>
+        <CssBaseline />
+            <Container maxWidth="lg">
+                <Header title={imagenCenter} sections={sections} />
+                <main>
+                    <MainFeaturedPost post={mainFeaturedPost} />
+                    <Grid container spacing={4}>
+                        {featuredPosts.map((post) => (
+                            <FeaturedPost key={post.title} post={post} />
+                        ))}
+                    </Grid>
+                    {/* <Grid container spacing={5} className={classes.mainGrid}>
+                        <Main title="From the firehose" posts={posts} />
+                        <Sidebar
+                            title={sidebar.title}
+                            description={sidebar.description}
+                            archives={sidebar.archives}
+                            social={sidebar.social}
+                        />
+                    </Grid> */}
+                </main>
+            </Container>
+            <Footer title="Silkroad Allianz" description="Servidor privado" />
+            </Fragment>
+    )
 }
 
-Main.propTypes = {
-    posts: PropTypes.array,
-    title: PropTypes.string,
-};
