591c44bebb508b79568f4a36cedd98649d619864
jellan <jellan_15@hotmail.com>
2021-07-03 17:26:50 -0400

    fixremove card action in card from politicsDetails

--- src/components/politics/PoliticsDetails.js
+++ src/components/politics/PoliticsDetails.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -28,8 +27,6 @@ export default function MediaCard() {
         <Fragment>
             <Box display="flex" justifyContent="center" alignItems="center" className={classes.box}>
                 <Card className={classes.root}>
-
-                    <CardActionArea>
                         <CardMedia
                             className={classes.media}
                             image="/static/images/cards/contemplative-reptile.jpg"
@@ -144,7 +141,6 @@ export default function MediaCard() {
                                 </p>
                             </Typography>
                         </CardContent>
-                    </CardActionArea>
                 </Card>
             </Box>
 
