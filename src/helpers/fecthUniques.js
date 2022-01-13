ecf0effddff316d8e28bafb537e9e868bfb78e87
jellan bozo <jellan.bozo0@walmart.com>
2022-01-13 20:25:25 -0300

    set enviroment viable from fetch unique kill

--- src/helpers/fecthUniques.js
+++ src/helpers/fecthUniques.js
@@ -1,5 +1,5 @@
 
 export const UseFetchGetUniquesKills = async () => {
-    const url = `http://localhost:3002/Unique/ListUniqueKill`;
+    const url = `${process.env.REACT_APP_API_URL}/Unique/ListUniqueKill`;
     return (await fetch(url, {mode:'cors', method:'GET'})).json();
 }
