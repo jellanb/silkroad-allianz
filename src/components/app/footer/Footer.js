976c0387f8ae2c5dc7fbcfa78f54a7673764a6b1
jellan bozo <jellan.bozo0@walmart.com>
2022-01-25 20:37:37 -0300

    add partners logo

--- src/components/app/footer/Footer.js
+++ src/components/app/footer/Footer.js
@@ -9,6 +9,12 @@ import WhatsApp from '@material-ui/icons/WhatsApp';
 import Discord from '@material-ui/icons/Chat';
 import Sidebar from './Siderbar';
 import Grid from '@material-ui/core/Grid';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import imagenElitepevpers from '../../../images/elitepvpers.png';
+import vsroplus from '../../../images/thumbnail_main.png'
+
 function Copyright() {
     return (
         <Typography variant="body2" align="center">
@@ -28,6 +34,15 @@ const useStyles = makeStyles((theme) => ({
         background: 'linear-gradient(360deg, #232121 30%, #1B1919 90%)',
         color: 'white'
     },
+    mediaDownload: {
+        height: 50,
+        width: 100
+    },
+    Card: {
+        maxWidth: 200,
+        backgroundColor: 'transparent',
+        paddingRight: '10%',
+    },
 }));
 
 const sidebar = {
@@ -38,6 +53,12 @@ const sidebar = {
         { name: 'Discord', icon: Discord, url: 'https://discord.gg/GRfHYPnwPp' },
     ],
 };
+
+const parnets = [
+    { name: 'elitepvpers', image:`${imagenElitepevpers}`, url:`https://www.elitepvpers.com/`},
+    { name: 'vplus', image:`${vsroplus}`, url:`http://vsroplus.com/`}
+]
+
 
 export default function Footer({ description, title }) {
     const classes = useStyles();
@@ -58,6 +79,15 @@ export default function Footer({ description, title }) {
                 <Typography variant="subtitle1" align="center">
                     POWERED BY VPS CHAIN FROM EEUU
                 </Typography>
+                <Typography variant="subtitle1" align="center">
+                    PARTNERS
+                    {parnets.map((container,index) =>
+                        <div className={classes.root} align={'center'}>
+                        <Grid>
+                            <Partner key={index} classes={classes} container={container} />
+                        </Grid>
+                        </div>)}
+                </Typography>
                 </Grid>
                 <Grid item lg={4} xs={6}>
                     <Sidebar
@@ -66,8 +96,27 @@ export default function Footer({ description, title }) {
                 </Grid>
             </Grid>
         </footer>
-    );
+    )
+
+    function Partner({classes, container}) {
+        console.log(classes);
+        console.log(container);
+        return (
+            <Grid item xl={2} lg={6}>
+                <Card className={classes.Card} style={{ border: "none", boxShadow: "none" }}>
+                    <CardActionArea href={`${container.url}`} target="_blank">
+                            <CardMedia
+                                className={classes.mediaDownload}
+                                image={container.image}
+                            />
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        )
+    }
 }
+
+
 
 Footer.propTypes = {
     description: PropTypes.string,
